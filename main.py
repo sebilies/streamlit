a6e6c2a9971eac22be8bf931ea1826aae563a1d2
SEBKHI ILIE <ilies.sebkhi@soummam-dz.com>
2025-08-02 11:07:42 +0100

    third commit

--- main.py
+++ main.py
@@ -6,23 +6,17 @@ st.header("This is the header")# Affiche un en-tête de niveau 1 (juste en desso
 st.markdown("This is the markdown")# Affiche un texte formaté en markdown (permet des styles comme gras, italique, listes, etc.)
 st.subheader("This is the subheader")# Affiche un sous-titre (niveau en dessous de header)
 st.caption("This is the caption")# Affiche une légende ou un texte d’accompagnement en petit (souvent pour des explications)
-st.code("x = 2021") # Affiche un bloc de code avec coloration syntaxique
-st.latex(r''' a+a r^1+a r^2+a r^3 ''')# Affiche une formule mathématique en LaTeX (utile pour les équations
+
 
 st.selectbox('Pick a fruit', ['Apple', 'Banana', 'Orange'])
 st.multiselect('Choose a planet', ['Jupiter', 'Mars', 'Neptune'])
 st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
 st.slider('Pick a number', 0, 50)
 
-st.selectbox('Pick a fruit', ['Apple', 'Banana', 'Orange'])
-st.multiselect('Choose a planet', ['Jupiter', 'Mars', 'Neptune'])
-st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
-st.slider('Pick a number', 0, 50)
 st.number_input('Pick a number', 0, 10)
 st.text_input('Email address')
 st.date_input('Traveling date')
 st.time_input('School time')
 st.text_area('Description')
-st.file_uploader('Upload a photo')
 st.color_picker('Choose your favorite color')
 
